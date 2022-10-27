c1806aa811ee1ebce36b2210176c715e78065f5d
Ekaterina Sergeeva <ekatandr002@mail.ru>
2022-10-27 18:03:09 +0300

    Step 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
+import greeting from '../src/cli.js';
+import readlineSync from 'readline-sync';
+
 console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
+//const greeting = greeting();
+
